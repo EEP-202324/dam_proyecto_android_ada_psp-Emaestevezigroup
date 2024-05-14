4b0d413736962c12e4ef8e3ab4e919c80b8f263b
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-14 06:40:57 +0200

    He renombrado las clases para más claridad

---
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.example.universidades.ui.Screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UniversityDetailScreen(universityId: String) {
+    // Aquí deberías obtener los detalles de la universidad con universityId
+    // Por ejemplo, puedes usar el ID para buscar la universidad en tu base de datos o en tu repositorio
+    // Luego puedes mostrar los detalles de la universidad en esta pantalla
+
+    // Temporalmente, solo mostraremos el ID de la universidad para demostración
+    Text(text = "University ID: $universityId", modifier = Modifier.padding(16.dp))
+}
---
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityListScreen.kt
@@ -0,0 +1,57 @@
+package com.example.universidades.ui.Screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.universidades.R
+import com.example.universidades.models.University
+
+@Composable
+fun UniversityListScreen(
+    universities: List<University>,
+    onUniversitySelected: (String) -> Unit,
+    onAddUniversityClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.university_list_title),
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(count = universities.size) { index ->
+                val university = universities[index]
+                UniversityListItem(university = university, onClick = onUniversitySelected)
+            }
+        }
+
+
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = onAddUniversityClicked,
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = stringResource(id = R.string.add_university_button_label))
+        }
+    }
+}
+
+@Composable
+fun UniversityListItem(university: University, onClick: (String) -> Unit) {
+    Button(onClick = { onClick(university.id.toString()) }) {
+        Text(text = university.name)
+    }
+}
+
+
