3d0afd0324d016498fe3f14b667731c39df100b5
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-14 22:00:57 +0200

    Modifico UniversityApp

---
+++ Android/app/src/main/java/com/example/universidades/UniversityApp.kt
@@ -0,0 +1,112 @@
+package com.example.universidades
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.universidades.ui.screen.AddUniversityScreen
+import com.example.universidades.ui.screen.StartScreen
+import com.example.universidades.ui.screen.UniversityDetailScreen
+import com.example.universidades.ui.screen.UniversityListScreen
+import com.example.universidades.viewmodels.UniversityUiState
+import com.example.universidades.viewmodels.UniversityViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UniversityApp(navController: NavHostController = rememberNavController()) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    val universityViewModel: UniversityViewModel = viewModel()
+    val universityUiState by universityViewModel.universityUiState.observeAsState()
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { UniversityTopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            UniversityContent(
+                universityUiState = universityUiState ?: UniversityUiState.Loading,
+                navController = navController,
+            )
+        }
+    }
+}
+
+
+@Composable
+fun UniversityTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = { Text(text = "Universidades") },
+        navigationIcon = {
+            IconButton(onClick = { /* Manejar la navegación hacia atrás o abrir el menú lateral */ }) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menú")
+            }
+        },
+        actions = {
+            // Agregar acciones adicionales aquí, como botones de búsqueda o de perfil
+            IconButton(onClick = { /* Acción 1 */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Buscar")
+            }
+        },
+        backgroundColor = MaterialTheme.colors.secondary, // Color de fondo de la barra
+        contentColor = MaterialTheme.colors.onPrimary, // Color del contenido de la barra
+        modifier = modifier
+    )
+}
+
+
+
+@Composable
+fun UniversityContent(universityUiState: UniversityUiState, navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "start") {
+        composable(route = "start") {
+            StartScreen(
+                onStartButtonClicked = { navController.navigate("university_list") }
+            )
+        }
+        composable(route = "university_list") {
+            UniversityListScreen(
+                universities = (universityUiState as? UniversityUiState.Success)?.universities ?: emptyList(),
+                onUniversitySelected = { universityId ->
+                    navController.navigate("university_detail/$universityId")
+                },
+                onAddUniversityClicked = { navController.navigate("add_university") }
+            )
+        }
+
+        composable(route = "university_detail/{universityId}") { backStackEntry ->
+            val universityId = backStackEntry.arguments?.getString("universityId")
+            if (universityId != null) {
+                UniversityDetailScreen(universityId = universityId)
+            } else {
+                Text(text = "Error: ID de universidad no proporcionado")
+            }
+        }
+
+        composable(route = "add_university") {
+            AddUniversityScreen(navController = navController)
+        }
+    }
+}
