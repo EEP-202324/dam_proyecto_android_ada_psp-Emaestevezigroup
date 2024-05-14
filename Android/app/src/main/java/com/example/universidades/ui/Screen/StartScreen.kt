662ba5510487b671f150db1b873752b279f106b5
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-14 06:42:30 +0200

    Nueva pantalla de inicio

---
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/StartScreen.kt
@@ -0,0 +1,35 @@
+package com.example.universidades.ui.Screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.universidades.R
+
+@Composable
+fun StartScreen(onStartButtonClicked: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.start_screen_title))
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onStartButtonClicked) {
+            Text(text = stringResource(R.string.start_button_label))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StartScreenPreview() {
+    StartScreen(onStartButtonClicked = {})
+}
