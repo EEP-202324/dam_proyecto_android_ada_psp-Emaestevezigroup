86a4bd8216c4f48dc5d775c3657d94501496e44d
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-17 20:03:58 +0200

    Detalles a mostrar

--- Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityDetailScreen.kt
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityDetailScreen.kt
@@ -1,5 +1,8 @@
 package com.example.universidades.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,11 +10,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun UniversityDetailScreen(universityId: String) {
-    // Aquí deberías obtener los detalles de la universidad con universityId
-    // Por ejemplo, puedes usar el ID para buscar la universidad en tu base de datos o en tu repositorio
-    // Luego puedes mostrar los detalles de la universidad en esta pantalla
-
-    // Temporalmente, solo mostraremos el ID de la universidad para demostración
-    Text(text = "University ID: $universityId", modifier = Modifier.padding(16.dp))
+fun UniversityDetailScreen(
+    universityId: Long,
+    name: String,
+    address: String,
+    phoneNumber: String,
+    email: String,
+    hasScholarship: Boolean,
+    location: String,
+    category: String
+) {
+    Column(Modifier.padding(16.dp)) {
+        Text(text = "University ID: $universityId")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Name: $name")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Address: $address")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Phone Number: $phoneNumber")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Email: $email")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Scholarship: $hasScholarship")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Location: $location")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Category: $category")
+    }
 }
