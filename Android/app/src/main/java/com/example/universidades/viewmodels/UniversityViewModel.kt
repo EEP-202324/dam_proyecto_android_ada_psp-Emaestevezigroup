8c1356d14504409ad0fda598573052477ee079b9
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-17 18:40:43 +0200

    Actualizo ViewModel

--- Android/app/src/main/java/com/example/universidades/viewmodels/UniversityViewModel.kt
+++ Android/app/src/main/java/com/example/universidades/viewmodels/UniversityViewModel.kt
@@ -8,36 +8,27 @@ import androidx.lifecycle.viewModelScope
 import com.example.universidades.api.UniversityApi
 import com.example.universidades.models.University
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class UniversityViewModel : ViewModel() {
     private val _universityUiState = MutableLiveData<UniversityUiState>()
     val universityUiState: LiveData<UniversityUiState> = _universityUiState
 
+    private val _errorMessage = MutableLiveData<String>()
+
     init {
-        Log.d("UniversityViewModel", "Before init***")
         loadUniversities()
-        Log.d("UniversityViewModel", "After init***")
     }
 
     private fun loadUniversities() {
         viewModelScope.launch {
             try {
-                // Show a loading state while fetching data
                 _universityUiState.value = UniversityUiState.Loading
-                Log.d("UniversityViewModel", "Fetching universities data from API...")
-
-                // Try to fetch universities from the API
                 val universities = UniversityApi.retrofitService.getAllUniversities()
-
-                // Log the fetched universities
-                Log.d("UniversityViewModel", "Universities fetched: $universities")
-
-                // Update the state with the loaded list of universities
                 _universityUiState.value = UniversityUiState.Success(universities)
             } catch (e: Exception) {
-                // Handle any errors that occur during data loading
-                Log.e("UniversityViewModel", "Error loading universities: ${e.message}", e)
-                _universityUiState.value = UniversityUiState.Error("Error loading universities: ${e.message}")
+                Log.e("UniversityViewModel", "Error cargando universidades: ${e.message}", e)
+                _universityUiState.value = UniversityUiState.Error("Error cargando universidades: ${e.message}")
             }
         }
     }
@@ -45,16 +36,22 @@ class UniversityViewModel : ViewModel() {
     fun createUniversity(university: University) {
         viewModelScope.launch {
             try {
-                val createdUniversity = UniversityApi.retrofitService.createUniversity(university)
-                Log.d("UniversityViewModel", "University created: $createdUniversity")
-                // Aquí puedes agregar la lógica necesaria para manejar el resultado de la creación de la universidad
+                UniversityApi.retrofitService.createUniversity(university)
             } catch (e: Exception) {
-                Log.e("UniversityViewModel", "Error creating university: ${e.message}", e)
-                // Aquí puedes agregar la lógica necesaria para manejar errores en la creación de la universidad
+                val errorMessage = when (e) {
+                    is HttpException -> {
+                        when (e.code()) {
+                            400 -> "El número de teléfono debe tener 9 dígitos o el email debe tener un formato válido."
+                            else -> "Error creando universidad: ${e.message}"
+                        }
+                    }
+                    else -> "Error creando universidad: ${e.message}"
+                }
+                _errorMessage.postValue(errorMessage)
+                Log.e("UniversityViewModel", errorMessage, e)
             }
         }
     }
-
 }
 
 sealed class UniversityUiState {
