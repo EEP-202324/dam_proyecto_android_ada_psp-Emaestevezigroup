77b80555027193608a56e3c16c90bf2b24c6d8cc
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-18 12:26:54 +0200

    Modifico botón

--- Android/app/src/main/java/com/example/universidades/ui/Screen/StartScreen.kt
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/StartScreen.kt
@@ -1,7 +1,6 @@
 package com.example.universidades.ui.screen
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,28 +9,42 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.universidades.R
 import androidx.compose.foundation.Image
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 
 @Composable
 fun StartScreen(onStartButtonClicked: () -> Unit) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(30.dp),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(R.drawable.logo),
             contentDescription = stringResource(R.string.logo_description),
-            modifier = Modifier.size(200.dp)
+            modifier = Modifier.size(250.dp)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(R.string.welcome_message), style = MaterialTheme.typography.h6)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onStartButtonClicked) {
-            Text(text = stringResource(R.string.start_button_label)) 
+        Text(
+            text = stringResource(R.string.welcome_message),
+            style = MaterialTheme.typography.h6
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+        OutlinedButton(
+            onClick = onStartButtonClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 70.dp)
+                .height(48.dp),
+            colors = ButtonDefaults.outlinedButtonColors(backgroundColor = MaterialTheme.colors.primary)
+        ) {
+            Text(
+                text = stringResource(R.string.start_button_label),
+                style = MaterialTheme.typography.button
+            )
         }
     }
 }
