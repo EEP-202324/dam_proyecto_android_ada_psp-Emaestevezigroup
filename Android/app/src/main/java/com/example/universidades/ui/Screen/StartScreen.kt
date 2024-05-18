f6019969982186afa93e4c9e4c8b3de787395a21
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-18 10:27:10 +0200

    Añado logo

--- Android/app/src/main/java/com/example/universidades/ui/Screen/StartScreen.kt
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/StartScreen.kt
@@ -7,29 +7,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.universidades.R
+import androidx.compose.foundation.Image
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun StartScreen(onStartButtonClicked: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(30.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.start_screen_title))
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = stringResource(R.string.logo_description),
+            modifier = Modifier.size(200.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = stringResource(R.string.welcome_message), style = MaterialTheme.typography.h6)
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = onStartButtonClicked) {
-            Text(text = stringResource(R.string.start_button_label))
+            Text(text = stringResource(R.string.start_button_label)) 
         }
     }
-}
-
-@Preview
-@Composable
-fun StartScreenPreview() {
-    StartScreen(onStartButtonClicked = {})
 }
