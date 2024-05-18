d06d041f1474b546f678796e13fcc340db2e273c
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-18 20:00:08 +0200

    Simplifico y añado botón de Actualizar

--- Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityDetailScreen.kt
+++ Android/app/src/main/java/com/example/universidades/ui/Screen/UniversityDetailScreen.kt
@@ -1,40 +1,52 @@
 package com.example.universidades.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.universidades.models.University
 
 @Composable
 fun UniversityDetailScreen(
-    universityId: Long,
-    name: String,
-    address: String,
-    phoneNumber: String,
-    email: String,
-    hasScholarship: Boolean,
-    location: String,
-    category: String
+    university: University,
+    onUpdateUniversityClicked: (University) -> Unit,
 ) {
     Column(Modifier.padding(16.dp)) {
-        Text(text = "University ID: $universityId")
+        Text(text = "University ID: ${university.id ?: "N/A"}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Name: $name")
+        Text(text = "Name: ${university.name}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Address: $address")
+        Text(text = "Address: ${university.address}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Phone Number: $phoneNumber")
+        Text(text = "Phone Number: ${university.phoneNumber}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Email: $email")
+        Text(text = "Email: ${university.email}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Scholarship: $hasScholarship")
+        Text(text = "Scholarship: ${university.hasScholarship}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Location: $location")
+        Text(text = "Location: ${university.location.name}")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Category: $category")
+        Text(text = "Category: ${university.category.name}")
+
+        Button(
+            onClick = { onUpdateUniversityClicked(university) },
+            modifier = Modifier.padding(top = 160.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Text(
+                text = "Actualizar",
+                fontSize = 14.sp,
+                style = MaterialTheme.typography.button,
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
     }
 }
