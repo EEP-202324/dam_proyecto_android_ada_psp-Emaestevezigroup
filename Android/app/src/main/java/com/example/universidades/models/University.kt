f1906b0b21d34bf3f68647b7edc8bdd32fd2353e
EmaEEP <145351175+Emaestevezigroup@users.noreply.github.com>
2024-05-19 20:13:34 +0200

    Añado el id

--- Android/app/src/main/java/com/example/universidades/models/University.kt
+++ Android/app/src/main/java/com/example/universidades/models/University.kt
@@ -3,6 +3,7 @@ package com.example.universidades.models
 import com.google.gson.annotations.SerializedName
 
 data class University(
+    @SerializedName("id") val id: Long?,
     @SerializedName("name") val name: String,
     @SerializedName("address") val address: String,
     @SerializedName("phoneNumber") val phoneNumber: String,
